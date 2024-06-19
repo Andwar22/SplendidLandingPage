55465056cdaa89b73b2d7a6cbd9861416c723c8c
Andwar22 <and.war22@gmail.com>
2024-06-19 18:47:55 +0700

    insert particle js

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,3 +25,83 @@
         darkModeToggle.addEventListener("change", toggleDarkMode);
     });
 // #endregion ======= DARK TOGGLE END
+
+// #region ========== PARTICLES 
+    const warna = '#29b765';
+
+    tsParticles.load("tsparticles", {
+        background: {
+            color: {
+                value: 'var(--bg-theme)'
+            }
+        },
+        fpsLimit: 120,
+        interactivity: {
+            events: {
+                onClick: {
+                    enable: true,
+                    mode: 'push'
+                },
+                onHover: {
+                    enable: true,
+                    mode: 'grab'
+                },
+            },
+            modes: {
+                bubble: {
+                    distance: 400,
+                    duration: 2,
+                    opacity: 0.8,
+                    size: 40
+                },
+                push: {
+                    quantity: 4
+                },
+                repulse: {
+                    distance: 200,
+                    duration: 0.4
+                },
+                grab: {
+                    distance: 400
+                }
+            }
+        },
+        particles: {
+            color: {
+                value: warna
+            },
+            links: {
+                color: warna,
+                distance: 150,
+                enable: true,
+                opacity: 0.5,
+                width: 1
+            },
+            move: {
+                direction: 'none',
+                enable: true,
+                outModes: 'out',
+                random: false,
+                speed: 2,
+                straight: false
+            },
+            number: {
+                density: {
+                    enable: true,
+                    value_area: 800
+                },
+                value: 200
+            },
+            opacity: {
+                value: 0.5
+            },
+            shape: {
+                type: 'circle'
+            },
+            size: {
+                value: { min: 1, max: 5 }
+            }
+        },
+        detectRetina: true,
+    });
+// #endregion ======= PARTICLES END
